108263771a05770f20c7f07fb600aec197a47c2a
hamstar0 <webutek@gmail.com>
2019-10-12 01:03:52 -0700

    v1.1.1 - Imported FloatInputElement via subclass

--- Config.cs
+++ Config.cs
@@ -1,3 +1,4 @@
+using HamstarHelpers.Classes.UI.ModConfig;
 using System;
 using System.ComponentModel;
 using Terraria;
@@ -5,6 +6,11 @@ using Terraria.ModLoader.Config;
 
 
 namespace PowerfulMagic {
+	class MyFloatInputElement : FloatInputElement { }
+
+
+
+
 	public class PowerfulMagicConfig : ModConfig {
 		public override ConfigScope Mode => ConfigScope.ServerSide;
 
@@ -13,6 +19,7 @@ namespace PowerfulMagic {
 
 		[Range( 0f, 50f )]
 		[DefaultValue(6f)]
+		[CustomModConfigItem( typeof( MyFloatInputElement ) )]
 		public float DamageScale = 6f;
 
 		[Range( 0f, 5f )]
