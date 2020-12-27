f25c62532c23b693c5da8fcdd9b2824503e5409f
hamstar0 <webutek@gmail.com>
2020-12-26 21:29:53 -0800

    v1.5.3.1 - Tweaked magic item tooltips

--- PowerfulMagic/MyItem_Tooltips.cs
+++ PowerfulMagic/MyItem_Tooltips.cs
@@ -23,13 +23,17 @@ namespace PowerfulMagic {
 				return;
 			}
 
+			string modName = "[c / FFFF88:" + PowerfulMagicMod.Instance.DisplayName + "] - ";
 			//int newDmg = Main.LocalPlayer.GetWeaponDamage( item );
 			int dmgPercent = (int)(dmgScale * 100f);
 			int manaPercent = (int)(config.Get<float>( nameof(PowerfulMagicConfig.WeaponManaConsumeMulitplier) ) * 100f);
 
-			var tip1 = new TooltipLine( this.mod, "PowerfulMagicDmgUpTip", "Magic increased "+dmgPercent+"% of base amount" );
+			string tip1Text = modName + "Magic increased " + dmgPercent + "% of base amount";
+			var tip1 = new TooltipLine( this.mod, "PowerfulMagicDmgUpTip", tip1Text );
 			tip1.overrideColor = Color.Lerp( Color.Black, Color.Lime, 0.8f + (0.2f * mymod.Oscillate) );
-			var tip2 = new TooltipLine( this.mod, "PowerfulMagicManaUpTip", "Mana use increased "+manaPercent+"% of base amount" );
+
+			string tip2Text = modName + "Mana use increased " + manaPercent + "% of base amount";
+			var tip2 = new TooltipLine( this.mod, "PowerfulMagicManaUpTip", tip2Text );
 			tip2.overrideColor = Color.Lerp( Color.Black, Color.Lime, 0.8f + (0.2f * mymod.Oscillate) );
 
 			tooltips.Insert( 1, tip1 );
