9f843c8ce9af649554fc7630bbcd9e03b576c339
hamstar0 <webutek@gmail.com>
2021-10-30 02:57:02 -0700

    v1.9.0 - Added tooltips for temperature items

--- PowerfulMagic/MyItem_Tooltips.cs
+++ PowerfulMagic/MyItem_Tooltips.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 
@@ -16,15 +18,43 @@ namespace PowerfulMagic {
 				return;
 			}
 
-			var mymod = (PowerfulMagicMod)this.mod;
 			var config = PowerfulMagicConfig.Instance;
 			if( config == null ) {
 				return;
 			}
 
 			string modName = "[c/FFFF88:" + PowerfulMagicMod.Instance.DisplayName + "] - ";
+
+			int tipIdx = 1;
+
+			this.ModifyTooltips_Magic( modName, tooltips, dmgScale.Value, ref tipIdx );
+
+			switch( item.type ) {
+			case ItemID.SpaceGun:
+			case ItemID.LaserRifle:
+				this.ModifyTooltips_Lasers( modName, item, tooltips, ref tipIdx );
+				this.ModifyTooltips_TemperatureIf( modName, item, tooltips, ref tipIdx );
+				break;
+			case ItemID.MeteorHelmet:
+			case ItemID.MeteorSuit:
+			case ItemID.MeteorLeggings:
+				this.ModifyTooltips_TemperatureIf( modName, item, tooltips, ref tipIdx );
+				break;
+			}
+		}
+
+
+		////////////////
+
+		private void ModifyTooltips_Magic(
+					string modName,
+					List<TooltipLine> tooltips,
+					float dmgScale,
+					ref int tipIdx ) {
+			var config = PowerfulMagicConfig.Instance;
+
 			//int newDmg = Main.LocalPlayer.GetWeaponDamage( item );
-			int dmgPercent = (int)(dmgScale.Value * 100f);
+			int dmgPercent = (int)(dmgScale * 100f);
 			int manaPercent = (int)(config.Get<float>( nameof(PowerfulMagicConfig.WeaponManaConsumeMulitplier) ) * 100f);
 
 			string tip1Text = modName + "Magic increased " + dmgPercent + "% of base amount";
@@ -35,8 +65,39 @@ namespace PowerfulMagic {
 			var tip2 = new TooltipLine( this.mod, "PowerfulMagicManaUpTip", tip2Text );
 			//tip2.overrideColor = Color.Lerp( Color.Black, Color.Lime, 0.8f + (0.2f * mymod.Oscillate) );
 
-			tooltips.Insert( 1, tip1 );
-			tooltips.Insert( 2, tip2 );
+			tooltips.Insert( tipIdx++, tip1 );
+			tooltips.Insert( tipIdx++, tip2 );
+		}
+
+
+		////
+
+		private void ModifyTooltips_Lasers(
+					string modName,
+					Item item,
+					List<TooltipLine> tooltips,
+					ref int tipIdx ) {
+			string tipText = modName + "Warning: May overheat if overused with Meteor Armor!";
+			var tip = new TooltipLine( this.mod, "PowerfulMagicLaserWarning", tipText );
+
+			tooltips.Insert( tipIdx++, tip );
+		}
+
+		private void ModifyTooltips_TemperatureIf(
+					string modName,
+					Item item,
+					List<TooltipLine> tooltips,
+					ref int tipIdx ) {
+			if( this.Temperature <= 0f ) {
+				return;
+			}
+
+			string tipText = modName + "Temperature percent until overhead: " + (int)this.Temperature;
+			var tip = new TooltipLine( this.mod, "PowerfulMagicTemp", tipText );
+
+			tip.overrideColor = Color.Lerp( Color.Lime, Color.Red, this.Temperature );
+
+			tooltips.Insert( tipIdx++, tip );
 		}
 	}
 }
